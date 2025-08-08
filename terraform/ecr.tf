7e439461cdf888596753c63dd7234a973b821bde
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-08 16:34:38 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,1 @@
-resource "aws_ecr_repository" "frontend" {
-  name                 = "frontend"
-  image_tag_mutability = "MUTABLE"
-}
 
-resource "aws_ecr_repository" "backend" {
-  name                 = "backend"
-  image_tag_mutability = "MUTABLE"
-}
