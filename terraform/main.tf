64e60167cd7bb565247977840640f8aeceff840c
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-08 13:41:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,7 +173,7 @@ resource "aws_db_instance" "postgres" {
   engine                 = "postgres"
   engine_version         = "13.7"
   instance_class         = "db.t3.micro"
-  name                   = var.db_name
+  db_name                   = var.db_name
   username               = var.db_username
   password               = var.db_password
   skip_final_snapshot    = true
