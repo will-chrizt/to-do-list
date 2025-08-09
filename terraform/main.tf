51d842d2cd82c71f6cba1e23f3ce6cae3df7238c
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-09 17:17:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -224,7 +224,7 @@ resource "kubernetes_secret" "db_credentials" {
     name = "db-credentials"
   }
 
-  string_data = {
+  data = {
     database_url = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.postgres.address}:5432/${var.db_name}"
   }
 
