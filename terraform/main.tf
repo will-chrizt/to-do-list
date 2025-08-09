e8749440c0499ea05affdf6b39f7f340377f87c2
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-09 17:09:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = ">= 2.11.0"
     }
   }
 }
+
 
 provider "aws" {
   region = var.aws_region
