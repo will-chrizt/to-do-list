63aed5087da447abd833b61c718f64a275c8d046
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-10 19:51:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -196,24 +196,21 @@ resource "aws_security_group" "rds_sg" {
 # -------------------------
 # Kubernetes provider (after cluster + nodes)
 # -------------------------
-data "aws_eks_cluster" "cluster" {
+data "aws_eks_cluster" "eks" {
   name = aws_eks_cluster.eks.name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.eks.name
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = aws_eks_cluster.eks.name
-  depends_on = [aws_eks_node_group.node_group] # ✅ Allowed here
+  depends_on = [aws_eks_node_group.node_group] # Wait until nodes are up
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 
 
 # -------------------------
